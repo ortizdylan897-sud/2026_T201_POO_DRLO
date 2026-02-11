7e886c009e8d8bdc5a6305e25515183170f9b505
DILAN <DILAN@192.168.8.156>
2026-02-11 14:24:59 -0600

    inicial

---
+++ src/main/java/com/mycompany/ti_201_2026_dilan_lule/Private.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ti_201_2026_dilan_lule;
+
+/**
+ *
+ * @author DILAN
+ */
+class Private {
+    
+}
---
+++ src/main/java/com/mycompany/ti_201_2026_dilan_lule/Student.java
@@ -0,0 +1,39 @@
+/*
+*2026 ITESS-TICS
+*PERIODO:ENERO-JUNIO
+*ASIGNATURA: PROGRAMACION ORTIENTADA A OBJETOS
+*DOCENTE: FRANCISCO JAVIER MONTESILLO PUENTE
+*DESCRIPCION: TEMA 1.2
+*ENTORNO DE DESARROLLO: 
+                      PRUEBA DEL ENTORNO DEL DESARROLLO Y GITHUB
+PROGRAMADOR:DRLO
+FECHA: 09 DE FERERO 2026 
+
+
+ */
+
+package com.mycompany.ti_201_2026_dilan_lule;
+
+
+public class Student  {
+    private String cotrolNumber;
+    private String name;
+    private String career;
+    private String gender;
+    
+    public Student (String controlNumber, String name, String career, String gender ) {
+      this.cotrolNumber = controlNumber;
+              this.name = name;
+              this.career = career;
+              this.gender = gender;
+              
+    }
+    
+}
+ 
+   
+    
+    
+
+   
+    
